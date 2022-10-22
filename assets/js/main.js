d0746a34a4cdb2f39cc4abf7e83af46e98286a09
eric <eric.tran@alpaca.vn>
2022-10-22 16:35:59 +0700

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,3 +24,55 @@ $(document).ready(function () {
       $('.mobile-search-advance').toggleClass('d-block')
    })
 })
+const filterPriceRange = document.querySelector(".filter-price-range");
+const filterPrefix = document.querySelector(".filter-prefix");
+const filterNetWork = document.querySelector(".filter-network");
+const filterTypeSim = document.querySelector(".filter-type-sim");
+const filterSort = document.querySelector(".filter-sort");
+const contentFilters = document.querySelector(".content-filters");
+
+// 1. filterPriceRange
+filterPriceRange.addEventListener("click", ()=>{
+   const show = contentFilters.classList.contains("active-filter");
+   {show ? contentFilters.classList.remove("active-filter") : contentFilters.classList.add("active-filter")}
+})
+filterPriceRange.addEventListener("mouseleave", ()=>{
+   const show = contentFilters.classList.contains("active-filter");
+   {show && contentFilters.classList.remove("active-filter");}
+})
+// 2. filterPriceRange
+filterPrefix.addEventListener("click", ()=>{
+   const show = contentFilters.classList.contains("active-filter");
+   {show ? contentFilters.classList.remove("active-filter") : contentFilters.classList.add("active-filter")}
+})
+filterPrefix.addEventListener("mouseleave", ()=>{
+   const show = contentFilters.classList.contains("active-filter");
+   {show && contentFilters.classList.remove("active-filter");}
+})
+// 3. filterPriceRange
+filterNetWork.addEventListener("click", ()=>{
+   const show = contentFilters.classList.contains("active-filter");
+   {show ? contentFilters.classList.remove("active-filter") : contentFilters.classList.add("active-filter")}
+})
+filterNetWork.addEventListener("mouseleave", ()=>{
+   const show = contentFilters.classList.contains("active-filter");
+   {show && contentFilters.classList.remove("active-filter");}
+})
+// 3. filterTypeSim
+filterTypeSim.addEventListener("click", ()=>{
+   const show = contentFilters.classList.contains("active-filter");
+   {show ? contentFilters.classList.remove("active-filter") : contentFilters.classList.add("active-filter")}
+})
+filterTypeSim.addEventListener("mouseleave", ()=>{
+   const show = contentFilters.classList.contains("active-filter");
+   {show && contentFilters.classList.remove("active-filter");}
+})
+// 4. filterSort
+filterSort.addEventListener("click", ()=>{
+   const show = contentFilters.classList.contains("active-filter");
+   {show ? contentFilters.classList.remove("active-filter") : contentFilters.classList.add("active-filter")}
+})
+filterSort.addEventListener("mouseleave", ()=>{
+   const show = contentFilters.classList.contains("active-filter");
+   {show && contentFilters.classList.remove("active-filter");}
+})
